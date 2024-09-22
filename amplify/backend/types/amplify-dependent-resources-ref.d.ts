9f34ae4d5de3aa6b66039b5fc9027a735ac5536f
JuanCarlosLopezHortelano <juancaloslhhellin@gmail.com>
2024-09-22 20:11:40 +0200

    COMMIT INICIAL

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifydev280a785e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
